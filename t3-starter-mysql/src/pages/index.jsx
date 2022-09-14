237e5fe2b1dc7bdc46bea19ca9c50c2ab917835e
Christopher Carpousis <carpousis@gmail.com>
2022-09-13 21:45:36 -0400

    initialized skatespot app

---
+++ t3-starter-mysql/src/pages/index.jsx
@@ -0,0 +1,46 @@
+// import { useSession } from "next-auth/react";
+
+
+// function Home() {
+//   const { isLoaded, data: session, status } = useSession({
+//   })
+
+//   if (status === "authenticated") {
+//     return <p>Signed in as {session.user.email}</p>
+//   }
+
+//   return (
+//     <main>
+//       This is the home page
+//     </main>
+//   );
+// }
+
+
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export default function Home() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApitKey: process.env.NEXT_PUBLIC_API_KEY,
+        nonce: process.env.NEXT_PUBLIC_API_KEY,
+    });
+if (!isLoaded) {
+    return (
+        <div>
+            Skating around...
+        </div>
+    )
+}
+        return <Map />;
+}
+
+function Map() {
+    const center = useMemo(() => ({lat: 38.9072, lng:77.0369}), []);
+    return <GoogleMap zoom={10} 
+    center = {center} 
+    mapContainerClassName="map-mapContainer"
+    >
+        <Marker position={{lat: 38.9072, lng:77.0369}} />
+    </GoogleMap>;
+}
